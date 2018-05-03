fb83f2a08c05be5fdcf8761cb5716afe6512ac18
nghuubaotrung <bin1302@gmail.com>
2018-05-03 16:25:13 +0900

    first connect with eth network

---
+++ reactEth.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.11;
+
+contract ReactExample {
+	address private owner;
+	string public you_awesome;
+	string private secret;
+
+	function ReactExample () public {
+		owner = msg.sender;
+		you_awesome = "You're awesome";
+		secret = "testing secret data";
+	}
+
+	function getSecret () public view returns (string) {
+		return secret;
+	}
+
+	function kill () public {
+		require (msg.sender == owner);
+		selfdestruct (owner);
+	}
+
+	function () public payable {
+		reve
+	}
+}
--- src/App.js
+++ src/App.js
@@ -3,16 +3,85 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super (props);
+    const MyContract = window.web3.eth.contract ([
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "kill",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getSecret",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "you_awesome",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	}
+]);
+
+    this.state = {
+      ContractInstance: MyContract.at('0x222492f119557e83e6db7166fb1060555dd60b3a')
+    }
+    this.querySecret = this.querySecret.bind(this);
+  }
+
+  querySecret () {
+    const { getSecret } = this.state.ContractInstance;
+
+    getSecret ((err, secret) => {
+      if (err) console.error('An error occured:::', err);
+      console.log('This is our contract secret:::', secret);
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Base System for React Ether Test</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <br />
+        <button onClick={ this.querySecret }> Querry Smart Contract Secret </button>
+        <br />
       </div>
     );
   }
