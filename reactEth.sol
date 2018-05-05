69272490ddfdc72bb38a2a27181e9e7bc729f2d5
nghuubaotrung <bin1302@gmail.com>
2018-05-05 15:10:13 +0900

    Smart Contract's state manipulation from React side

--- reactEth.sol
+++ reactEth.sol
@@ -1,26 +1,36 @@
 pragma solidity ^0.4.11;
 
 contract ReactExample {
-	address private owner;
-	string public you_awesome;
-	string private secret;
+    address private owner;
+    string public you_awesome;
+    string private secret;
+    string private state;
 
-	function ReactExample () public {
-		owner = msg.sender;
-		you_awesome = "You're awesome";
-		secret = "testing secret data";
-	}
+    function ReactExample () public {
+        owner = msg.sender;
+        you_awesome = "You're awesome";
+        secret = "testing data";
+        state = "init state";
+    }
 
-	function getSecret () public view returns (string) {
-		return secret;
-	}
+    function getSecret () public view returns (string) {
+        return secret;
+    }
 
-	function kill () public {
-		require (msg.sender == owner);
-		selfdestruct (owner);
-	}
+    function getState () public view returns (string) {
+        return state;
+    }
 
-	function () public payable {
-		reve
-	}
+    function setState (string newState) public payable {
+        state = newState;
+    }
+
+    function kill () public {
+        require (msg.sender == owner);
+        selfdestruct (owner);
+    }
+
+    function () public payable {
+        revert ();
+    }
 }
--- src/App.js
+++ src/App.js
@@ -16,9 +16,48 @@ class App extends Component {
 		"type": "function"
 	},
 	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "newState",
+				"type": "string"
+			}
+		],
+		"name": "setState",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
 		"constant": true,
 		"inputs": [],
 		"name": "getSecret",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getState",
 		"outputs": [
 			{
 				"name": "",
@@ -42,24 +81,16 @@ class App extends Component {
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"payable": true,
-		"stateMutability": "payable",
-		"type": "fallback"
 	}
 ]);
 
     this.state = {
-      ContractInstance: MyContract.at('0x222492f119557e83e6db7166fb1060555dd60b3a')
+      ContractInstance: MyContract.at('0x3ef910a7761995f2a44f2dfeaf3da9444b832a63'),
+      contractState: ''
     }
     this.querySecret = this.querySecret.bind(this);
+    this.queryContractState = this.queryContractState.bind(this);
+    this.handleContractStateSubmit = this.handleContractStateSubmit.bind (this);
   }
 
   querySecret () {
@@ -69,6 +100,33 @@ class App extends Component {
       if (err) console.error('An error occured:::', err);
       console.log('This is our contract secret:::', secret);
     })
+  }
+
+  queryContractState () {
+    const { getState } = this.state.ContractInstance;
+
+    getState ((err, state) => {
+      if (err) console.error('An error occured:::', err);
+      console.log('This is our contract state:::', state);
+    })
+  }
+
+  handleContractStateSubmit (event) {
+    event.preventDefault ();
+
+    const { setState } = this.state.ContractInstance;
+    const { contractState: newState } = this.state;
+
+    setState (
+      newState,
+      {
+        gas: 300000,
+        from: window.web3.eth.accounts[0],
+        value: window.web3.toWei (0.01, 'ether')
+      }, (err, result) => {
+        console.log ('Smart contract state is changing.');
+      }
+    )
   }
 
   render() {
@@ -82,6 +140,19 @@ class App extends Component {
         <br />
         <button onClick={ this.querySecret }> Querry Smart Contract Secret </button>
         <br />
+        <button onClick={ this.queryContractState }> Querry Smart Contract State </button>
+        <br />
+        <br />
+        <br />
+        <form onSubmit={ this.handleContractStateSubmit }>
+          <input
+            type="text"
+            name="state-change"
+            placeholder="Enter new state..."
+            value ={ this.state.contractState }
+            onChange={ event => this.setState ({ contractState: event.target.value }) } />
+          <button type="submit"> Submit </button>
+        </form>
       </div>
     );
   }
