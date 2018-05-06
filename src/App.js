399481910846ffbf2966c862a7cf2c9b48ad485f
nghuubaotrung <bin1302@gmail.com>
2018-05-06 20:08:16 +0900

    added Toban's Account Smart Contract logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './nn_logo.png';
 import './App.css';
 
 class App extends Component {
@@ -7,30 +7,37 @@ class App extends Component {
     super (props);
     const MyContract = window.web3.eth.contract ([
 	{
-		"anonymous": false,
+		"constant": false,
 		"inputs": [
 			{
-				"indexed": false,
-				"name": "result",
-				"type": "bool"
+				"name": "_name",
+				"type": "string"
 			}
 		],
-		"name": "ExperimentComplete",
-		"type": "event"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "kill",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
+		"name": "createAccount",
+		"outputs": [
+			{
+				"name": "_accountId",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
 		"constant": false,
-		"inputs": [],
-		"name": "setExperimentInMotion",
+		"inputs": [
+			{
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "editAccount",
 		"outputs": [
 			{
 				"name": "",
@@ -43,22 +50,17 @@ class App extends Component {
 	},
 	{
 		"constant": false,
-		"inputs": [
+		"inputs": [],
+		"name": "resetAccount",
+		"outputs": [
 			{
-				"name": "newState",
-				"type": "string"
+				"name": "",
+				"type": "uint256"
 			}
 		],
-		"name": "setState",
-		"outputs": [],
-		"payable": true,
-		"stateMutability": "payable",
+		"payable": false,
+		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"payable": true,
-		"stateMutability": "payable",
-		"type": "fallback"
 	},
 	{
 		"inputs": [],
@@ -69,11 +71,11 @@ class App extends Component {
 	{
 		"constant": true,
 		"inputs": [],
-		"name": "getSecret",
+		"name": "getAccountCount",
 		"outputs": [
 			{
-				"name": "",
-				"type": "string"
+				"name": "_count",
+				"type": "uint256"
 			}
 		],
 		"payable": false,
@@ -82,39 +84,16 @@ class App extends Component {
 	},
 	{
 		"constant": true,
-		"inputs": [],
-		"name": "getState",
-		"outputs": [
+		"inputs": [
 			{
-				"name": "",
-				"type": "string"
+				"name": "_id",
+				"type": "uint256"
 			}
 		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "pseudoRandomResult",
+		"name": "getAccountName",
 		"outputs": [
 			{
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "you_awesome",
-		"outputs": [
-			{
-				"name": "",
+				"name": "_name",
 				"type": "string"
 			}
 		],
@@ -125,111 +104,139 @@ class App extends Component {
 ]);
 
     this.state = {
-      ContractInstance: MyContract.at('0x7e7280f0b555f79c2400a2c4eecc2df51b88d430'),
-      contractState: ''
+      ContractInstance: MyContract.at('0x021aac0ca930eb8a56d26e0228bb29bc3de89b78'),
     }
-    this.querySecret = this.querySecret.bind(this);
-    this.queryContractState = this.queryContractState.bind(this);
-    this.handleContractStateSubmit = this.handleContractStateSubmit.bind (this);
-
-    this.queryConditionResult = this.queryConditionResult.bind (this);
-    this.activateExperiment = this.activateExperiment.bind (this);
-
-    this.state.event = this.state.ContractInstance.ExperimentComplete();
+    this.queryResetAccount = this.queryResetAccount.bind(this);
+    this.queryGetAccountCount = this.queryGetAccountCount.bind(this);
+    this.handleGetAccountNameSubmit = this.handleGetAccountNameSubmit.bind (this);
+    this.handleContractAccountSubmit = this.handleContractAccountSubmit.bind (this);
+    this.handleEditAccountSubmit = this.handleEditAccountSubmit.bind (this);
   }
 
-  querySecret () {
-    const { getSecret } = this.state.ContractInstance;
+  queryResetAccount () {
+    const { resetAccount } = this.state.ContractInstance;
 
-    getSecret ((err, secret) => {
+    resetAccount ((err) => {
       if (err) console.error('An error occured:::', err);
-      console.log('This is our contract secret:::', secret);
+      console.log('All accounts have been reset');
     })
   }
 
-  queryContractState () {
-    const { getState } = this.state.ContractInstance;
+  queryGetAccountCount () {
+    const { getAccountCount } = this.state.ContractInstance;
 
-    getState ((err, state) => {
+    getAccountCount ((err, _count) => {
       if (err) console.error('An error occured:::', err);
-      console.log('This is our contract state:::', state);
+      console.log('This is our account count:::', _count);
     })
   }
 
-  handleContractStateSubmit (event) {
+  handleGetAccountNameSubmit (event) {
     event.preventDefault ();
 
-    const { setState } = this.state.ContractInstance;
-    const { contractState: newState } = this.state;
+    const { getAccountName } = this.state.ContractInstance;
+    const { accountId: _id } = this.state;
 
-    setState (
-      newState,
+    getAccountName (
+      _id, (err, result) => {
+        console.log ('Account name is:::', result);
+      }
+    )
+  }
+
+  handleContractAccountSubmit (event) {
+    event.preventDefault ();
+
+    const { createAccount } = this.state.ContractInstance;
+    const { newAccount: _name } = this.state;
+
+    createAccount (
+      _name,
       {
         gas: 300000,
         from: window.web3.eth.accounts[0],
         value: window.web3.toWei (0.01, 'ether')
       }, (err, result) => {
-        console.log ('Smart contract state is changing.');
+        console.log ('Smart contract account is adding.');
       }
     )
   }
 
-  queryConditionResult () {
-    const { pseudoRandomResult } = this.state.ContractInstance;
+  handleEditAccountSubmit (event) {
+    event.preventDefault ();
 
-    pseudoRandomResult ((err, result) => {
-      console.log ('This is the smart contract conditional::::', result);
-    })
-  }
+    const { editAccount } = this.state.ContractInstance;
+    const { accountId: _id } = this.state;
+    const { accountName: _name } = this.state;
 
-  activateExperiment () {
-    const { setExperimentInMotion } = this.state.ContractInstance;
-
-    setExperimentInMotion ({
-      gas: 300000,
-      from: window.web3.eth.accounts[0],
-      value: window.web3.toWei (0.01, 'ether')
-    }, (err, result) => {
-      console.log ('Experiment to determine true or false set in motion.');
-    })
+    editAccount (
+      _id,
+      _name,
+      {
+        gas: 300000,
+        from: window.web3.eth.accounts[0],
+        value: window.web3.toWei (0.01, 'ether')
+      }, (err, result) => {
+        console.log ('Smart contract account is adding.');
+      }
+    )
   }
 
   render() {
-      this.state.event.watch ((err, event) => {
-      if (err) console.error ('An error occured::::', err);
-      console.log ('This is the event::::', event);
-      console.log ('This is the experiment result::::', event.args.result);
-    });
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Base System for React Ether Test</h1>
+          <h1 className="App-title">Toban System Prototype</h1>
         </header>
         <br />
         <br />
-        <button onClick={ this.querySecret }> Querry Smart Contract Secret </button>
-        <br />
-        <button onClick={ this.queryContractState }> Querry Smart Contract State </button>
+        <button onClick={ this.queryResetAccount }> Reset All Account </button>
         <br />
         <br />
         <br />
-        <form onSubmit={ this.handleContractStateSubmit }>
+        <button onClick={ this.queryGetAccountCount }> Get Account Count </button>
+        <br />
+        <br />
+        <form onSubmit={ this.handleGetAccountNameSubmit }>
           <input
             type="text"
             name="state-change"
-            placeholder="Enter new state..."
-            value ={ this.state.contractState }
-            onChange={ event => this.setState ({ contractState: event.target.value }) } />
-          <button type="submit"> Submit </button>
+            placeholder="Enter account id..."
+            value ={ this.state.accountId }
+            onChange={ event => this.setState ({ accountId: event.target.value }) } />
+          <button type="submit"> Search For Account </button>
         </form>
         <br />
         <br />
-        <button onClick={ this.queryConditionResult }> Query Smart Contract Conditional Result </button>
+        <form onSubmit={ this.handleContractAccountSubmit }>
+          <input
+            type="text"
+            name="state-change"
+            placeholder="Enter new account name..."
+            value ={ this.state.newAccount }
+            onChange={ event => this.setState ({ newAccount: event.target.value }) } />
+          <button type="submit"> Create New Account </button>
+        </form>
         <br />
         <br />
-        <button onClick={ this.activateExperiment }> Start Experiment on Smart Contract </button>
+        <br />
+        <br />
+        <form onSubmit={ this.handleEditAccountSubmit }>
+          <input
+            type="text"
+            name="state-change"
+            placeholder="Enter accountId"
+            value ={ this.state.accountId }
+            onChange={ event => this.setState ({ accountId: event.target.value }) } />
+          <input
+            type="text"
+            name="state-change"
+            placeholder="Enter New Account Name"
+            value ={ this.state.accountName }
+            onChange={ event => this.setState ({ accountName: event.target.value }) } />
+          <button type="submit"> Update Account Name </button>
+        </form>
       </div>
     );
   }
